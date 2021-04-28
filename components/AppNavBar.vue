137688a837b73c14e7fca67d5ee9a1bc5d7c9516
jasonkhew96 <29531167+JasonKhew96@users.noreply.github.com>
2021-04-28 21:56:00 +0800

    Clean up

--- components/AppNavBar.vue
+++ components/AppNavBar.vue
@@ -2,25 +2,6 @@
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
       <b-navbar-brand to="/">Bangumi Timeline</b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item href="#" disabled>Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown text="Lang" right disabled>
-            <b-dropdown-item href="#">zh-Hans</b-dropdown-item>
-            <b-dropdown-item href="#">zh-Hant</b-dropdown-item>
-            <b-dropdown-item href="#">JP</b-dropdown-item>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
     </b-navbar>
   </div>
 </template>
@@ -28,6 +9,3 @@
 <script>
 export default {}
 </script>
-
-<style>
-</style>
