a002f562978ed856bebd846fd5c585d5b815ac61
jasonkhew96 <29531167+JasonKhew96@users.noreply.github.com>
2021-05-13 21:50:00 +0800

    Add bilibili overseas index

--- components/AppNavBar.vue
+++ components/AppNavBar.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand to="/">Bangumi Timeline</b-navbar-brand>
+      <b-navbar-brand to="/">Bangumi</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/bilibili">BiliBili</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
   </div>
 </template>
---
+++ pages/bilibili.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <app-nav-bar />
+    <b-container>
+      <app-bilibili-overseas />
+    </b-container>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import AppNavBar from '~/components/AppNavBar.vue'
+import AppBilibiliOverseas from '~/components/AppBilibiliOverseas.vue'
+export default Vue.extend({
+  components: {
+    AppNavBar,
+    AppBilibiliOverseas,
+  },
+})
+</script>
