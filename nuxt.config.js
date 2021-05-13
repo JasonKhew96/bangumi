b9fd2ae11e5abc35f8d2549c1907526947b34833
jasonkhew96 <29531167+JasonKhew96@users.noreply.github.com>
2021-05-13 21:49:14 +0800

    Change name

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Bangumi Timeline',
+    title: 'Bangumi',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
