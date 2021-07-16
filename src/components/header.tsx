bf7e1d08515097a2f0e6e166472adecd4994196f
jasonkhew96 <29531167+JasonKhew96@users.noreply.github.com>
2021-07-16 21:33:50 +0800

    Add appbar and drawer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,42 +1,75 @@
 import * as React from "react"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  SwipeableDrawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@material-ui/core"
+import MenuIcon from "@material-ui/icons/Menu"
+import DirectionsBoatIcon from "@material-ui/icons/DirectionsBoat"
+import FlightIcon from "@material-ui/icons/Flight"
 
-const Header = ({ siteTitle }: any) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+export default function Header({ siteTitle }: any): React.ReactElement {
+  const [state, setState] = React.useState({
+    drawer: false,
+  })
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (event && event.type === "keydown") {
+        return
+      }
+
+      setState({ ...state, drawer: open })
+    }
+
+  return (
+    <header>
+      <div>
+        <AppBar>
+          <Toolbar>
+            <IconButton color="inherit" onClick={toggleDrawer(true)}>
+              <MenuIcon />
+            </IconButton>
+            <Typography>{siteTitle}</Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <SwipeableDrawer
+        open={state.drawer}
+        onOpen={toggleDrawer(true)}
+        onClose={toggleDrawer(false)}
+      >
+        <List>
+          <ListItem button key="嗶哩嗶哩（港澳臺）">
+            <ListItemIcon>
+              <DirectionsBoatIcon />
+            </ListItemIcon>
+            <ListItemText primary="嗶哩嗶哩（港澳臺）" />
+          </ListItem>
+          <ListItem button key="哔哩哔哩（东南亚）">
+            <ListItemIcon>
+              <FlightIcon />
+            </ListItemIcon>
+            <ListItemText primary="哔哩哔哩（东南亚）" />
+          </ListItem>
+        </List>
+      </SwipeableDrawer>
+    </header>
+  )
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+// Header.propTypes = {
+//   siteTitle: PropTypes.string,
+// }
 
-export default Header
+// Header.defaultProps = {
+//   siteTitle: ``,
+// }
+
+// export default Header
