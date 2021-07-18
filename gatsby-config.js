0580bef0796a30b0008b18f067d4087ee654ee65
jasonkhew96 <29531167+JasonKhew96@users.noreply.github.com>
2021-07-18 17:03:53 +0800

    Update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Bangumi`,
+    description: `A webpage providing users bangumi list and timetable`,
+    author: `@JasonKhew96`,
+    siteUrl: `https://bangumi.jasonkhew96.eu.org/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
