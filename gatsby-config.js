15136f851f57900c0180452dd92c13311360ae42
jasonkhew96 <29531167+JasonKhew96@users.noreply.github.com>
2021-07-18 23:31:45 +0800

    Enable PWA + Offlne plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
     `gatsby-plugin-material-ui`,
     `gatsby-plugin-cname`,
   ],
