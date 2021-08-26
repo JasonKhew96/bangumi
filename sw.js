b3797a0b0103489e25827e251d0eefff95beb38b
jasonkhew96 <29531167+JasonKhew96@users.noreply.github.com>
2021-08-26 20:58:52 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1ac0652217c2ca5e9ed2.js"
+    "url": "webpack-runtime-4374bff54595e70e087f.js"
   },
   {
     "url": "framework-3e406ca7ce9502ac0e9f.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "68c897822fe3fbf5238a043349aed480"
+    "revision": "f886a0fac2c3dc505de50800b43c8837"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b8ed67b77776cbcd0090.js"
