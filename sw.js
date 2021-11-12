2006336d873128b1c86f6438783a249bb5820762
JasonKhew96 <JasonKhew96@users.noreply.github.com>
2021-11-12 11:09:18 +0000

    deploy: 46a1d1248e8dfb348e1f7e4588bc238ce4b170d2

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-354197794f8db08a9363.js"
+    "url": "webpack-runtime-273cfbf1535b847261f1.js"
   },
   {
     "url": "framework-3e406ca7ce9502ac0e9f.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4472f5a7165f425e4397643fb25b1a63"
+    "revision": "49bf92333a321b3f23e5fe081dfaad84"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b8ed67b77776cbcd0090.js"
