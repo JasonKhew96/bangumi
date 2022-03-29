8c41d145614389218b0e7fe36229d0157f589100
Jason_Khew <29531167+JasonKhew96@users.noreply.github.com>
2022-03-29 12:28:02 +0800

    Disable gatsby-plugin-material-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
-    `gatsby-plugin-material-ui`,
+    // `gatsby-plugin-material-ui`,
     `gatsby-plugin-cname`,
     {
       resolve: `gatsby-plugin-google-gtag`,
