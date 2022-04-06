2ae82ed3c307d03459b03c6ec9b6e8889d31ff3b
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2022-04-06 14:52:52 +0800

    Simplify graphql query

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,13 +8,12 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
 import Header from "./header"
 import "./layout.css"
 import { Container } from "@mui/material"
 
 const Layout = ({ children }: any) => {
-  const data = useStaticQuery(graphql`
+  const { site } = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
         buildTime(formatString: "YYYY-MM-DD hh:mm a z")
@@ -26,8 +25,14 @@ const Layout = ({ children }: any) => {
   `)
 
   return (
-    <div style={{ height: `100vh`, display: `flex`, flexDirection: `column` }}>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+    <div
+      style={{
+        height: `100vh`,
+        display: `flex`,
+        flexDirection: `column`,
+      }}
+    >
+      <Header siteTitle={site.siteMetadata?.title || `Title`} />
       <Container>
         <main>{children}</main>
       </Container>
@@ -44,7 +49,7 @@ const Layout = ({ children }: any) => {
         <a href="https://www.gatsbyjs.com">Gatsby</a>
         {` & `}
         <a href="https://mui.com/">Material-UI</a>
-        {` (Build time: ` + data.site.buildTime + `)`}
+        {` (Build time: ` + site.buildTime + `)`}
       </footer>
     </div>
   )
