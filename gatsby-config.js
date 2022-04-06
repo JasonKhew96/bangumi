217482ce8f636556d908ecf62190fba837effa2a
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2022-04-06 14:51:43 +0800

    Update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require(`path`)
+
 module.exports = {
   siteMetadata: {
     title: `Bangumi`,
@@ -12,7 +14,14 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `background`,
+        path: path.join(__dirname, `src`, `background`),
       },
     },
     `gatsby-transformer-sharp`,
