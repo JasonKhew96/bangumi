9ea5de6cf6300ee95f85fac6e76f67d30cb7f709
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2022-08-27 12:34:15 +0800

    update: bilibili overseas

--- tools/bilibili.go
+++ tools/bilibili.go
@@ -23,6 +23,135 @@ import (
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
+func (s *Scraper) updateBilibiliOverseas() error {
+	log.Println("updating bilibili overseas")
+
+	datas, err := models.Bilibilis(models.BilibiliWhere.UpMid.EQ(null.Int64From(11783021))).All(context.Background(), s.db)
+	if err != nil && err != sql.ErrNoRows {
+		return err
+	}
+
+	values := url.Values{}
+	for _, old := range datas {
+		values.Set("season_id", strconv.FormatInt(old.SeasonID, 10))
+		apiUrl := API_BILIBILI_SEASON + "?" + values.Encode()
+		resp, err := s.do(http.MethodGet, apiUrl, nil)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			return errors.New("status code error: " + resp.Status)
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		bilibiliRoot := &bilibili.SeasonResult{}
+		err = easyjson.Unmarshal(body, bilibiliRoot)
+		if err != nil {
+			return err
+		}
+
+		if bilibiliRoot.Code != 0 {
+			if err != sql.ErrNoRows {
+				log.Printf("bilibili delete [%d][%s]", old.SeasonID, old.Title.String)
+				if _, err := old.Delete(context.Background(), s.db); err != nil {
+					return err
+				}
+			}
+			time.Sleep(SLEEP_INTERVAL)
+			continue
+		}
+
+		result := bilibiliRoot.Result
+
+		var areas []string
+		for _, area := range result.Areas {
+			areas = append(areas, area.Name)
+		}
+		pubTime, _ := time.Parse(TIMESTAMP_FORMAT, result.Publish.PubTime)
+		new := &models.Bilibili{
+			Actors:      null.StringFrom(result.Actors),
+			Areas:       null.StringFrom(strings.Join(areas, "|")),
+			Alias:       null.StringFrom(result.Alias),
+			Cover:       null.StringFrom(result.Cover),
+			Evaluate:    null.StringFrom(result.Evaluate),
+			JPTitle:     null.StringFrom(result.JpTitle),
+			MediaID:     int64(result.MediaID),
+			IsVip:       result.Payment != nil,
+			PubTime:     null.TimeFrom(pubTime),
+			RatingCount: null.Int64From(int64(result.Rating.Count)),
+			RatingScore: null.Float64From(result.Rating.Score),
+			Copyright:   null.StringFrom(result.Rights.Copyright),
+			SeasonID:    int64(result.SeasonID),
+			SeasonTitle: null.StringFrom(result.SeasonTitle),
+			SeasonType:  int64(result.SeasonType),
+			SeriesTitle: null.StringFrom(result.SeriesTitle),
+			SquareCover: null.StringFrom(result.SquareCover),
+			Coins:       null.Int64From(int64(result.Stat.Coins)),
+			Danmakus:    null.Int64From(int64(result.Stat.Danmakus)),
+			Views:       null.Int64From(int64(result.Stat.Views)),
+			Style:       null.StringFrom(strings.Join(result.Style, "|")),
+			Title:       null.StringFrom(strings.TrimSpace(result.Title)),
+			UpMid:       null.Int64From(int64(result.UpInfo.Mid)),
+		}
+
+		if err != sql.ErrNoRows &&
+			old.Actors == new.Actors &&
+			old.Areas == new.Areas &&
+			old.Alias == new.Alias &&
+			old.Cover == new.Cover &&
+			old.Evaluate == new.Evaluate &&
+			old.JPTitle == new.JPTitle &&
+			old.MediaID == new.MediaID &&
+			old.IsVip == new.IsVip &&
+			old.PubTime == new.PubTime &&
+			old.RatingCount == new.RatingCount &&
+			old.RatingScore == new.RatingScore &&
+			old.Copyright == new.Copyright &&
+			old.SeasonTitle == new.SeasonTitle &&
+			old.SeasonType == new.SeasonType &&
+			old.SeriesTitle == new.SeriesTitle &&
+			old.SquareCover == new.SquareCover &&
+			old.Coins == new.Coins &&
+			old.Danmakus == new.Danmakus &&
+			old.Views == new.Views &&
+			old.Style == new.Style &&
+			old.Title == new.Title &&
+			old.UpMid == new.UpMid {
+			time.Sleep(SLEEP_INTERVAL)
+			continue
+		}
+
+		if err != sql.ErrNoRows {
+			changelog, err := diff.Diff(old, new)
+			if err != nil {
+				return err
+			}
+			log.Printf("bilibili diff [%d][%s]", new.SeasonID, new.Title.String)
+			for _, change := range changelog {
+				if change.Type == diff.UPDATE {
+					if len(change.Path) > 0 && (change.Path[0] == "UpdatedAt" || change.Path[0] == "CreatedAt") {
+						continue
+					}
+				}
+				log.Printf(`  [%s][%s] "%v" -> "%v"`, change.Type, change.Path[0], change.From, change.To)
+			}
+		} else {
+			log.Printf("bilibili new [%d][%s]", new.SeasonID, new.Title.String)
+		}
+
+		err = new.Upsert(context.Background(), s.db, true, []string{"season_id"}, boil.Blacklist("created_at"), boil.Infer())
+		if err != nil {
+			return err
+		}
+
+		time.Sleep(SLEEP_INTERVAL)
+	}
+	return nil
+}
+
 func (s *Scraper) scrapeBilibili() error {
 	log.Println("Scraping bilibili...")
 
