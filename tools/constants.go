a2ef96ac475120d68bb73ff7b331dabab992cd03
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2024-09-30 23:57:54 +0800

    fix: animad

--- tools/constants.go
+++ tools/constants.go
@@ -4,7 +4,7 @@ import "time"
 
 const (
 	DEFAULT_USER_AGENT          = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
-	API_ANIGAMER_LIST           = "https://api.gamer.com.tw/mobile_app/anime/v1/list.php"
+	API_ANIGAMER_LIST           = "https://api.gamer.com.tw/anime/v1/anime_list.php"
 	API_BANGUMI_BILIBILI_SEASON = "https://bangumi.bilibili.com/view/web_api/season"
 	API_BILIBILI_SEASON         = "https://api.bilibili.com/pgc/view/web/season"
 	TIMESTAMP_FORMAT            = "2006-01-02 15:04:05"
